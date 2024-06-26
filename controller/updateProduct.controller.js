002f1e23689359e3187fc1ce174f56bed75e7e8a
Manase44 <fikopersempre@gmail.com>
2024-06-26 13:32:28 +0300

    implement the updating a record route

--- controller/updateProduct.controller.js
+++ controller/updateProduct.controller.js
@@ -1,5 +1,37 @@
-const updateProduct = (req, res) => {
-    res.send("updating a product")
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const updateProduct = async(req, res) => {
+    const productId = req.params.id;
+    const {productThumbnail, productTitle, productDescription, productCost, onOffer} = req.body;
+
+    try {
+        const findTheProductToBeUpdated = await prisma.products.findUnique({
+            where: {
+                id: productId
+            }
+        })
+        if (!findTheProductToBeUpdated) {
+            res.status(200).json({ok:false, message: "The product to be updated was not found"})
+        }else{
+            const updatingProduct = await prisma.products.update({
+                where: {
+                    id : productId
+                },
+                data: {
+                    productThumbnail,
+                    productTitle,
+                    productDescription,
+                    productCost,
+                    onOffer
+                }
+            })
+            res.status(200).json({ok:true, message:"The product was updated successfully"})
+        }
+        
+    } catch (error) {
+        res.status(500).json({ok:false, message: "Something is wrong"})
+    }
 }
 
 export default updateProduct;
