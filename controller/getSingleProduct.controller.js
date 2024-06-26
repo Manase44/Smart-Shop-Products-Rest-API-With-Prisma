72615b646a95e58baa0ca5f6be3105b7f537e884
Manase44 <fikopersempre@gmail.com>
2024-06-26 13:20:35 +0300

    implement the getting a specific product route

--- controller/getSingleProduct.controller.js
+++ controller/getSingleProduct.controller.js
@@ -1,5 +1,24 @@
-const getSingleProduct = (req, res) => {
-    res.send("getting a single product")
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const getSingleProduct = async(req, res) => {
+    const productId = req.params.id
+    try {
+        const gettingSingleProduct = await prisma.products.findUnique({
+            where:{
+                id: productId
+            }
+        })
+
+        if (!gettingSingleProduct) {
+            res.status(200).json({ok:true, message: "The product was not found"})
+        } else {
+            res.status(200).json({ok:true, message: gettingSingleProduct})
+        }
+        
+    } catch (error) {
+        res.status(500).json({ok:false, message: "Something is wrong"})
+    }
 }
 
 export default getSingleProduct;
--- controller/gettingAllProducts.controller.js
+++ controller/gettingAllProducts.controller.js
@@ -3,11 +3,11 @@ const prisma = new PrismaClient()
 
 const getAllProducts = async(req, res) => {
     try {
-        const allUsers = await prisma.products.findMany()
-        if (allUsers.length === 0) {
+        const gettingAllProducts = await prisma.products.findMany()
+        if (gettingAllProducts.length === 0) {
             res.status(200).json({ok:true, message: "The product store is empty"})
         } else {
-            res.status(200).json({ok: true, message: allUsers})
+            res.status(200).json({ok: true, message: gettingAllProducts})
         }
     } catch (error) {
         res.status(500).json({ok:false, message: "Something is wrong"})
