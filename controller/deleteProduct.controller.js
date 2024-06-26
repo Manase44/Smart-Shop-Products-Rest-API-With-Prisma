2669f86362b504eb0e8fa61b9e700c389979d3e5
Manase44 <fikopersempre@gmail.com>
2024-06-26 13:03:03 +0300

    implemented the delete product route

--- controller/deleteProduct.controller.js
+++ controller/deleteProduct.controller.js
@@ -1,5 +1,18 @@
-const deleteProduct = (req, res) => {
-    res.send("deleting products")
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const deleteProduct = async(req, res) => {
+    const productId = req.params.id
+    try {
+        const deletingProduct =  await prisma.products.delete({
+            where:{
+                id : productId
+            }
+        })
+        res.status(200).json({ok:true, message: "The product was deleted successfully"})
+    } catch (error) {
+        res.status(500).json({ok:false, message:"Something is wrong"})
+    }
 }
 
 export default deleteProduct;
